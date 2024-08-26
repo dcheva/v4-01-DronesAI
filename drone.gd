54f82fc8f875e738c6e18f05c90644afa5f54425
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-27 02:27:12 +0300

    up

--- drone.gd
+++ drone.gd
@@ -2,14 +2,30 @@ extends CharacterBody2D
 
 @export var speed := 200
 
-
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var dir_x := Input.get_axis("ui_left", "ui_right")
-	if dir_x:
-		velocity.x = dir_x * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
+	var velocity_to := get_input_vetor2d() * speed * delta
+	if velocity_to.length() > 1:
+		# Moving
+		$AnimationPlayer.play("Move")
+	else: 
+		# Chilling
+		$AnimationPlayer.play("idle")
+	velocity = lerp(velocity, velocity_to, delta*4)
+	position += velocity
 	move_and_slide()
+	
+func get_input_vetor2d() -> Vector3:
+	var vector := Vector3.ZERO
+	if Input.is_action_pressed("move_forward"):
+		speed_to = max_speed * acceleration
+	if Input.is_action_pressed("move_backward"):
+		speed_to = max_speed * breaking
+	if Input.is_action_pressed("move_up"):
+		speed_to = max_speed * accelerations
+	if Input.is_action_pressed("move_down"):
+		speed_to = max_speed * breaking
+	if Input.is_action_pressed("move_left"):
+		steer_to = max_steer
+	if Input.is_action_pressed("move_right"):
+		steer_to = -max_steer
+	return vector
