60a76b327125b08ea0e56deb46a25e07b1a23793
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-27 18:08:29 +0300

    Tested parallax than Stacked on func get_tested_values3

--- drone.gd
+++ drone.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 @export var Yspeed := 1.0/3.0
 @export var Zspeed := 1.0/1.3
 @export var screen := Vector2i(1280, 540)
-@export var max_length3 := Vector3i(1120, -1000, 480)
 @export var scale_k := 10
 var scale_to := 1.0
 var velocity3 := Vector3.ZERO
@@ -24,14 +23,13 @@ func _physics_process(delta: float) -> void:
 	
 	# LERP veloctity 3d and change position in memory
 	velocity3 = lerp(velocity3, velocity3_to, delta * 4)
-	
-	# Parallax on Y axis
-	var s: float = -max_length3.y / (-max_length3.y + scale_k * sqrt(scale_k * -position3.y))
-	scale_to = s
-	self.scale = Vector2(scale_to, scale_to)
-	
 	get_tested_values3()
 	position3 += velocity3
+	
+	# Parallax on Y axis
+	var s: float = screen.y / (screen.y + scale_k * sqrt(scale_k * -position3.y))
+	scale_to = s
+	self.scale = Vector2(scale_to, scale_to)
 	
 	# Convert Vector2 to Vector 3 and change position on screen
 	velocity = vector3_to_vector2(velocity3) * scale_to
@@ -45,24 +43,7 @@ func _physics_process(delta: float) -> void:
 ## Test the position3 + velocity3 are in the bounds and are not collided
 func get_tested_values3() -> void:
 # Correct velocity3 and add warning $AnimationPlayer.play("Warning, Damage")
-	var p3test := position3 + velocity3
-	var velocity3_ := velocity3
-	if p3test.y < max_length3.y / scale_to:
-		velocity3_.y += 1
-	if p3test.y > 0:
-		velocity3_.y -= 1
-	if p3test.x > max_length3.x / scale_to:
-		velocity3_.x -= 1
-	if p3test.x < 0:
-		velocity3_.x += 1
-	if p3test.z > max_length3.z / scale_to:
-		velocity3_.z -= 1
-	if p3test.z < 0:
-		velocity3_.z += 1
-		
-	if velocity3_ != velocity3:
-		velocity3 = velocity3_
-		$AnimationPlayer.play("Damage")
+	pass
 	
 ## Returns Vector3i(x:right,-y:forward, z:up)
 func get_input_vetor3i() -> Vector3i:
